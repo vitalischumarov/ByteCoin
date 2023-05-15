783fa9bc513df94be27c01281781ea6e5112bb00
Vitali Schumarov <vitalischumarov@Vitalis-MacBook-Air-2.fritz.box>
2023-05-15 20:52:56 +0200

    first commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Vitali Schumarov on 15.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
