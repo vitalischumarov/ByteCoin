105c7c1592a983146fc99243f77806fae1073690
Vitali Schumarov <vitalischumarov@Vitalis-MacBook-Air-2.fritz.box>
2023-05-15 21:12:56 +0200

    picker View implemented and added function

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -7,16 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    let coinManager = CoinManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.delegate = self
     }
-
-
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate  {
+//    how many columns should be displayed
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        currencyLabel.text = coinManager.currencyArray[row]
+        return coinManager.currencyArray[row]
+    }
+    
+}
---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,16 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Vitali Schumarov on 15.05.23.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
